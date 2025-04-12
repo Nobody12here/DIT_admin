4e9a27b73256543b240a5ac4146033086b650e91
Nobody12here <uraja01313@gmail.com>
2025-04-12 19:59:03 +0500

    Updated allowed hosts

--- DIT_admin/settings.py
+++ DIT_admin/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ptff_!e-vx@)79j%06iwc)(^5qs$p64vvm(b4ko-22*)01gxgg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['38.242.137.199']
 
 
 # Application definition
