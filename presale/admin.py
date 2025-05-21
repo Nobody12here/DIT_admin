b934d214f2a5a6010903120b6cef2d66ff954e3f
Nobody12here <uraja01313@gmail.com>
2025-05-21 12:23:36 +0500

    Initialize project with Python version, README, main script, and dependency management

--- presale/admin.py
+++ presale/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
 from presale.models import Presale
-# Register your models here.
 
-admin.site.register(Presale)
+@admin.register(Presale)
+class PresaleAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'receiver_address',
+        'crypto_currency',
+        'dit_amount',
+        'usdt_amount',
+        'purchase_date',
+        'tokens_delivered'
+    )
+    
+    list_filter = ('crypto_currency', 'tokens_delivered', 'purchase_date')  # Optional filters on sidebar
+    search_fields = ('receiver_address', 'crypto_currency')  # Optional search bar
+    ordering = ('-purchase_date',)  # Most recent purchases first
