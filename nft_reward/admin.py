de452f0accb5fec983d79ba439c5c01e29118db4
Nobody12here <uraja01313@gmail.com>
2025-06-02 22:07:01 +0500

    Updated admin interface

--- nft_reward/admin.py
+++ nft_reward/admin.py
@@ -1,4 +1,19 @@
 from django.contrib import admin
 from .models import NFTReward
-# Register your models here.
-admin.site.register(NFTReward)
+
+
+@admin.register(NFTReward)
+class NFTRewardAdmin(admin.ModelAdmin):
+    list_display = (
+        "email",
+        "wallet_address",
+        "nft_type",
+        "dit_amount",
+        "reward_collection_date",
+        "reward_sent",
+    )
+    list_filter = ("nft_type", "reward_sent", "reward_collection_date")
+    search_fields = ("email", "wallet_address")
+    list_editable = ("reward_sent",)
+    readonly_fields = ("reward_collection_date",)
+    ordering = ("-reward_collection_date",)
