a486525134842f415d7ac1626e55f35689c6abb0
Nobody12here <uraja01313@gmail.com>
2025-06-30 10:58:35 +0500

    updated reward sent button

--- presale/admin.py
+++ presale/admin.py
@@ -4,18 +4,24 @@ from unfold.admin import ModelAdmin
 
 admin.site.site_header = "Diamond Token Admin"
 
+
 @admin.register(Presale)
 class PresaleAdmin(ModelAdmin):
     list_display = (
-        'id',
-        'receiver_address',
-        'crypto_currency',
-        'dit_amount',
-        'usdt_amount',
-        'purchase_date',
-        'tokens_delivered'
+        "id",
+        "receiver_address",
+        "crypto_currency",
+        "dit_amount",
+        "usdt_amount",
+        "purchase_date",
+        "tokens_delivered",
     )
-    
-    list_filter = ('crypto_currency', 'tokens_delivered', 'purchase_date')  # Optional filters on sidebar
-    search_fields = ('receiver_address', 'crypto_currency')  # Optional search bar
-    ordering = ('-purchase_date',)  # Most recent purchases first
+
+    list_filter = (
+        "crypto_currency",
+        "tokens_delivered",
+        "purchase_date",
+    )  # Optional filters on sidebar
+    search_fields = ("receiver_address", "crypto_currency")  # Optional search bar
+    ordering = ("-purchase_date",)  # Most recent purchases first
+    list_editable = ("tokens_delivered",)
