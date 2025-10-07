bb7b19a9611bf8d8be9624e6e070676023a4d647
Nobody12here <uraja01313@gmail.com>
2025-10-07 13:31:59 +0500

    added fields all

--- membership/serializers.py
+++ membership/serializers.py
@@ -5,5 +5,5 @@ from membership.models import Membership
 class MembershipSerializer(ModelSerializer):
     class Meta:
         model = Membership
-        
+        fields = '__all__'
         read_only_fields = ["id", "purchase_date"]
