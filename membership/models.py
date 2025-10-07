6ba48dc7cf925d627c21b418629e6fe313364b1e
Nobody12here <uraja01313@gmail.com>
2025-10-07 13:27:02 +0500

    added quantity and email

--- membership/models.py
+++ membership/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class Membership(models.Model):
     usdt_amount = models.DecimalField(max_digits=20, decimal_places=8)
     crypto_currency = models.CharField(max_length=50)
+    email= models.EmailField(blank=True,null=True)
+    quantity = models.PositiveIntegerField(blank=True ,null=True)
     purchase_date = models.DateTimeField(auto_now_add=True)
     receiver_address = models.CharField(max_length=255)
     membership_added = models.BooleanField(default=False)
--- membership/serializers.py
+++ membership/serializers.py
@@ -5,11 +5,5 @@ from membership.models import Membership
 class MembershipSerializer(ModelSerializer):
     class Meta:
         model = Membership
-        fields = [
-            "usdt_amount",
-            "crypto_currency",
-            "purchase_date",
-            "receiver_address",
-            "membership_added",
-        ]
+        
         read_only_fields = ["id", "purchase_date"]
