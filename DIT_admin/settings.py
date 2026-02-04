85fdafdb6107741cfe7c69891ec74afeefc4aade
Nobody12here <uraja01313@gmail.com>
2026-02-04 21:14:48 +0500

    Refactor settings.py for consistency: unify string quotes and format ALLOWED_HOSTS for readability

--- DIT_admin/settings.py
+++ DIT_admin/settings.py
@@ -27,22 +27,33 @@ UNFOLD = {
 }
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 CORS_ALLOW_ALL_ORIGINS = True
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ptff_!e-vx@)79j%06iwc)(^5qs$p64vvm(b4ko-22*)01gxgg'
+SECRET_KEY = "django-insecure-ptff_!e-vx@)79j%06iwc)(^5qs$p64vvm(b4ko-22*)01gxgg"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['38.242.137.199','localhost','127.0.0.1','http://localhost:5173/','https://dit-presale.vercel.app/','ditadmin.duckdns.org']
+ALLOWED_HOSTS = [
+    "38.242.137.199",
+    "localhost",
+    "127.0.0.1",
+    "http://localhost:5173/",
+    "https://dit-presale.vercel.app/",
+    "ditadmin.duckdns.org",
+    "https://test-admin.diamond-club.io",
+    "http://localhost:5432",
+    "https://test-community.diamond-club.io",
+    "http://localhost:1234",
+]
 
 
 # Application definition
- 
+
 
 INSTALLED_APPS = [
     "unfold",  # before django.contrib.admin
@@ -53,64 +64,64 @@ INSTALLED_APPS = [
     "unfold.contrib.guardian",  # optional, if django-guardian package is used
     "unfold.contrib.simple_history",  # optional, if django-simple-history package is used
     "django.contrib.admin",  # required
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'corsheaders',
-    'drf_yasg',
-    'rest_framework',
-    'presale',
-    'event',
-    'external_users',
-    'nft_reward',
-    'membership',
-    'donation'  
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "corsheaders",
+    "drf_yasg",
+    "rest_framework",
+    "presale",
+    "event",
+    "external_users",
+    "nft_reward",
+    "membership",
+    "donation",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'DIT_admin.urls'
+ROOT_URLCONF = "DIT_admin.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'DIT_admin.wsgi.application'
+WSGI_APPLICATION = "DIT_admin.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'dit_db',             # your PostgreSQL database name
-        'USER': 'postgres',              # your PostgreSQL username
-        'PASSWORD': 'Saitama12here.',      # your PostgreSQL password
-        'HOST': '38.242.137.199',             # or your remote IP if using a VPS
-        'PORT': '5432',                  # default PostgreSQL port
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "dit_db",  # your PostgreSQL database name
+        "USER": "postgres",  # your PostgreSQL username
+        "PASSWORD": "Saitama12here.",  # your PostgreSQL password
+        "HOST": "38.242.137.199",  # or your remote IP if using a VPS
+        "PORT": "5432",  # default PostgreSQL port
     }
 }
 
@@ -120,16 +131,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -137,9 +148,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -149,9 +160,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = '/home/root/DIT_admin/static'
+STATIC_URL = "static/"
+STATIC_ROOT = "/home/root/DIT_admin/static"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
